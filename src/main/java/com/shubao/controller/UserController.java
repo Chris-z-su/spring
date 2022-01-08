4f6eddd40228f8ef04f3ca918d9319dfb2487f12
chris <chris_z_su@163.com>
2022-01-08 16:51:51 +0800

    Spring MVC简介

--- src/main/java/com/shubao/controller/UserController.java
+++ src/main/java/com/shubao/controller/UserController.java
@@ -1,52 +1,15 @@
 package com.shubao.controller;
 
-import com.shubao.config.SpringConfiguration;
-import com.shubao.service.UserService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class UserController {
 
-    public static void main(String[] args) {
-//        testApplication();
-//        testAnnotation();
-        testConguration();
+    @RequestMapping("/quick")
+    public String save(){
+        System.out.println("UserController.save() running...");
+        return "success.jsp";
     }
 
-    /**
-     * 测试通过注解的方式配置类
-     */
-    private static void testConguration(){
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        UserService userService = applicationContext.getBean(UserService.class);
-        userService.save();
-    }
-
-    private static void testAnnotation(){
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserService userService = applicationContext.getBean(UserService.class);
-        userService.save();
-//        applicationContext.close();
-    }
-
-    /**
-     * 学习Spring配置文件注入方式
-     */
-    private static void testApplication(){
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("E:\\Javademo\\spring\\src\\main\\resources\\applicationContext.xml");
-//        UserService userService = (UserService) applicationContext.getBean("userService");
-        UserService userService = applicationContext.getBean(UserService.class);
-        userService.save();
-
-        /*
-         Exception in thread "main" java.lang.NullPointerException
-         at com.shubao.service.impl.UserServiceImpl.save(UserServiceImpl.java:16)
-         at com.shubao.controller.UserController.main(UserController.java:14)
-         */
-//        UserService userService = new UserServiceImpl();
-//        userService.save();
-    }
 }
