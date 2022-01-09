fc5c23ea06131ebabc194fa1d248c86111c9fe94
chris <chris_z_su@163.com>
2022-01-10 00:02:30 +0800

    Spring mvc 组件解析

--- src/main/java/com/shubao/controller/UserController.java
+++ src/main/java/com/shubao/controller/UserController.java
@@ -2,14 +2,17 @@ package com.shubao.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/quick")
-    public String save(){
+    @RequestMapping(value = "/quick", method = RequestMethod.GET, params = {"username"})
+    public String save(String username){
         System.out.println("UserController.save() running...");
-        return "success.jsp";
+        System.out.println("username = " + username);
+        return "/success.jsp";
     }
 
 }
