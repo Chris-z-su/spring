6fd0a996e414d454d9cab9ce919ea10aff94ba5b
chris-z-su <chris_z_su@163.com>
2022-02-28 21:23:07 +0800

    基于XML方式实现Spring AOP

--- src/main/java/com/shubao/aop/Target.java
+++ src/main/java/com/shubao/aop/Target.java
@@ -3,6 +3,7 @@ package com.shubao.aop;
 public class Target implements TargetInterface {
     @Override
     public void save() {
+//        int i = 1/0;
         System.out.println("save() running...");
     }
 }
