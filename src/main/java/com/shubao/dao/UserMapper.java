a3ad49747bb886cf1de0538290795bbec231d58e
chris <chris_z_su@163.com>
2022-03-13 12:02:36 +0800

    mybatis动态sql之if、foreach

--- src/main/java/com/shubao/dao/UserMapper.java
+++ src/main/java/com/shubao/dao/UserMapper.java
@@ -10,5 +10,8 @@ public interface UserMapper {
     List<User> findAllForMybatis() throws IOException;
 
     User findOneForMybatis(int id) throws IOException;
-    
+
+    List<User> findByCondition(User user);
+
+    List<User> findByIds(List<Long> ids);
 }
--- src/test/java/com/shubao/test/UserServiceTest.java
+++ src/test/java/com/shubao/test/UserServiceTest.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceTest {
@@ -27,11 +28,32 @@ public class UserServiceTest {
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
 
         //查询所有
-        List<User> userList = userMapper.findAllForMybatis();
-        System.out.println("userList = " + userList);
+//        List<User> userList = userMapper.findAllForMybatis();
+//        System.out.println("userList = " + userList);
 
         //根据id注解查询
-        User user = userMapper.findOneForMybatis(1);
-        System.out.println("user = " + user);
+//        User user = userMapper.findOneForMybatis(1);
+//        System.out.println("user = " + user);
+
+        //多条件查询
+//        User user = new User();
+//        user.setId(1L);
+//        user.setUsername("zhangsan");
+//        user.setPassword("123");
+//
+//        List<User> userList = userMapper.findByCondition(user);
+//        System.out.println("userList = " + userList);
+
+
+        //模拟ids的数据
+        List<Long> ids = new ArrayList<Long>();
+        ids.add(1L);
+        ids.add(12L);
+        ids.add(13L);
+
+        List<User> userList = userMapper.findByIds(ids);
+        System.out.println("userList = " + userList);
+
+
     }
 }
