399c268f1be10e5fde26a612ffc27c5bc23d6b9a
chris <chris_z_su@163.com>
2022-03-18 11:43:53 +0800

    add JavaTest.java file

---
+++ src/test/java/com/shubao/test/JavaTest.java
@@ -0,0 +1,48 @@
+package com.shubao.test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class JavaTest {
+
+    @Test
+    public void test1(){
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    @Test
+    public void test2(){
+        List<String> list = null;
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    @Test
+    public void test3(){
+        int[] nums = {1,2,3,1};
+        int[] nums2 = new int[3];
+        int[] nums3 = new int[]{1,2,3};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]){
+                    System.out.println(true);
+                }
+            }
+        }
+        System.out.println(false);
+    }
+
+    @Test
+    public void test4(){
+        HashMap hashMap = new HashMap();
+        System.out.println(hashMap);
+    }
+
+}
